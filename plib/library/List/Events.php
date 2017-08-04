e44441eb2237092473eea31c3783e4a80ac2476d
Frank Roth <frankred@web.de>
2017-08-04 12:09:49 +0200

    Lists and API extracted to seperate folder library

---
+++ plib/library/List/Events.php
@@ -0,0 +1,107 @@
+<?php
+// Copyright 1999-2017. Plesk International GmbH.
+
+class Modules_UptimeRobot_List_Events
+{
+
+    public static function getList($monitors, $view, $request)
+    {
+        $data = [];
+        foreach ($monitors as &$monitor) {
+            foreach ($monitor->logs as &$log) {
+                $data[] = [
+                    'column-1' => self::getHumandReadableEventTypeText($log->type),
+                    'column-2' => '<a href="'.$monitor->url.'" target="_blank">'.$monitor->url.'</a>',
+                    'column-3' => self::getHumandReadableDateTimeText($log->datetime),
+                    'column-4' => $log->reason->detail,
+                    'column-5' => self::getHumandReadableDurationText($log->duration)
+                ];                
+            }
+        }
+
+        $sortBy = 'column-3';
+
+        $options = [
+            'pageable' => false,
+            'defaultItemsPerPage' => 100,
+            'defaultSortField' => $sortBy,
+            'defaultSortDirection' => pm_View_List_Simple::SORT_DIR_UP,
+            'searchable' => false
+        ];
+
+        $eventsList = new pm_View_List_Simple($view, $request, $options);
+        $eventsList->setData($data);
+        $eventsList->setColumns([
+            'column-1' => [
+                'title' => pm_Locale::lmsg('overviewEventColEvent'),
+                'sortable' => true,
+                'searchable' => false,
+                'noEscape' => true
+            ], 
+            'column-2' => [
+                'title' =>  pm_Locale::lmsg('overviewEventColMonitor'),
+                'sortable' => true,
+                'searchable' => false,
+                'noEscape' => true
+            ], 
+            'column-3' => [
+                'title' =>  pm_Locale::lmsg('overviewEventColDateTime'),
+                'sortable' => true,
+                'searchable' => false
+            ],
+            'column-4' => [
+                'title' =>  pm_Locale::lmsg('overviewEventColReason'), 
+                'sortable' => true,
+                'searchable' => false
+            ],
+            'column-5' => [
+                'title' => pm_Locale::lmsg('overviewEventColDuration'),
+                'sortable' => true,
+                'searchable' => false
+            ]
+        ]);
+        $eventsList->setDataUrl(array('action' => 'eventslist-data'));
+
+        return $eventsList;
+    }
+
+    private static function getHumandReadableEventTypeText($type)
+    {
+        $type = intval($type);
+
+        switch ($type) {
+            case 1:
+                return '<span class="event eventOffline"></span>'.pm_Locale::lmsg('overviewEventOffline');
+            case 2:
+                return '<span class="event eventOnline"></span>'.pm_Locale::lmsg('overviewEventOnline');                
+            case 98:
+                return '<span class="event eventStarted"></span>'.pm_Locale::lmsg('overviewEventStarted');
+        }
+
+        return 'Unknown Event Type';
+    }
+
+    private static function getHumandReadableDateTimeText($dateTime)
+    {
+        return date('Y-m-d H:i:s', $dateTime);
+    }
+
+    public static function getHumandReadableDurationText($durationInSeconds)
+    {
+        $init = $durationInSeconds;
+
+        $hours = floor($init / 3600);
+        $minutes = floor(($init / 60) % 60);
+        $seconds = $init % 60;
+
+        $output = '';
+
+        if($hours < 10){ $output .= '0'; }
+        $output .= $hours.'h, ';
+        
+        if($minutes < 10){ $output .= '0'; }
+        $output .= $minutes.'m';
+
+        return $output;
+    }
+}
---
+++ plib/library/List/Monitors.php
@@ -0,0 +1,75 @@
+<?php
+// Copyright 1999-2017. Plesk International GmbH.
+
+class Modules_UptimeRobot_List_Monitors
+{
+
+    public static function getList($monitors, $view, $request)
+    {
+        $data = [];
+        foreach ($monitors as &$monitor) {
+            $data[] = [
+                'column-1' => $monitor->id,
+                'column-2' => '<a href="'.$monitor->url.'" target="_blank">'.$monitor->url.'</a>',
+                'column-3' => ' ',
+                'column-4' => $monitor->uptime[24].'%',
+                'column-5' => $monitor->uptime[24*60].'%',
+                'column-6' => $monitor->uptime[24*360].'%'
+            ];
+        }
+        
+        $options = [
+            'pageable' => false,
+            'defaultItemsPerPage' => 100
+        ];
+
+        $monitorsList = new pm_View_List_Simple($view, $request, $options);
+        $monitorsList->setData($data);
+        $monitorsList->setColumns([
+            'column-1' => [
+                'title' => 'ID',
+                'noEscape' => true,
+                'searchable' => false
+            ], 
+            
+            'column-2' => [
+                'title' => 'URL',
+                'noEscape' => true,
+                'sortable' => true,
+                'searchable' => false
+            ],
+
+            'column-3' => [
+                'title' => 'Uptime: ',
+                'noEscape' => true,
+                'sortable' => false,
+                'searchable' => false
+            ],
+
+            'column-4' => [
+                'title' => 'last 24 '.pm_Locale::lmsg('overviewMonitorsHours'),
+                'noEscape' => true,
+                'searchable' => false,
+                'sortable' => false
+            ],
+
+            'column-5' => [
+                'title' => 'last 60 '.pm_Locale::lmsg('overviewMonitorsDays'),
+                'noEscape' => true,
+                'searchable' => false,
+                'sortable' => false
+            ],
+
+            'column-6' => [
+                'title' => 'last 360 '.pm_Locale::lmsg('overviewMonitorsDays'),
+                'noEscape' => true,
+                'searchable' => false,
+                'sortable' => false
+            ]
+        ]);
+        $monitorsList->setDataUrl(array('action' => 'monitorslist-data'));
+
+
+        return $monitorsList;
+    }
+}
