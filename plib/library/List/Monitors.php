987ac78f54ea03f3c353579f464a07682556a0be
Frank Roth <frankred@web.de>
2017-08-17 10:24:01 +0200

    Removed clickable link if monitor is from type=ping; Internationalization refactored.

--- plib/library/List/Monitors.php
+++ plib/library/List/Monitors.php
@@ -17,11 +17,12 @@ class Modules_UptimeRobot_List_Monitors
         foreach ($monitors as &$monitor) {
             $data[] = [
                 'column-1' => $monitor->id,
-                'column-2' => '<a href="'.$monitor->url.'" target="_blank">'.$monitor->url.'</a>',
-                'column-3' => ' ',
-                'column-4' => $monitor->uptime[24].'%',
-                'column-5' => $monitor->uptime[24 * 60].'%',
-                'column-6' => $monitor->uptime[24 * 360].'%'
+                'column-2' => self::getHumandReadableMonitorType($monitor->type),
+                'column-3' => self::getHumandReadableURL($monitor->type, $monitor->url),
+                'column-4' => ' ',
+                'column-5' => $monitor->uptime[24].'%',
+                'column-6' => $monitor->uptime[24 * 60].'%',
+                'column-7' => $monitor->uptime[24 * 360].'%'
             ];
         }
 
@@ -35,41 +36,48 @@ class Modules_UptimeRobot_List_Monitors
         $monitorsList->setColumns(
             [
                 'column-1' => [
-                    'title'      => 'ID',
+                    'title'      => pm_Locale::lmsg('overviewMonitorID'),
                     'noEscape'   => true,
                     'searchable' => false
                 ],
 
                 'column-2' => [
-                    'title'      => 'URL',
+                    'title'      => pm_Locale::lmsg('overviewMonitorType'),
                     'noEscape'   => true,
                     'sortable'   => true,
                     'searchable' => false
                 ],
 
                 'column-3' => [
-                    'title'      => 'Uptime: ',
+                    'title'      =>  pm_Locale::lmsg('overviewMonitorURL'),
+                    'noEscape'   => true,
+                    'sortable'   => true,
+                    'searchable' => false
+                ],
+
+                'column-4' => [
+                    'title'      => pm_Locale::lmsg('overviewMonitorUptime'),
                     'noEscape'   => true,
                     'sortable'   => false,
                     'searchable' => false
                 ],
 
-                'column-4' => [
-                    'title'      => 'last 24 '.pm_Locale::lmsg('overviewMonitorsHours'),
-                    'noEscape'   => true,
-                    'searchable' => false,
-                    'sortable'   => false
-                ],
-
                 'column-5' => [
-                    'title'      => 'last 60 '.pm_Locale::lmsg('overviewMonitorsDays'),
+                    'title'      => pm_Locale::lmsg('overviewMonitorLast24Hours'),
                     'noEscape'   => true,
                     'searchable' => false,
                     'sortable'   => false
                 ],
 
                 'column-6' => [
-                    'title'      => 'last 360 '.pm_Locale::lmsg('overviewMonitorsDays'),
+                    'title'      => pm_Locale::lmsg('overviewMonitorLast60days'),
+                    'noEscape'   => true,
+                    'searchable' => false,
+                    'sortable'   => false
+                ],
+
+                'column-7' => [
+                    'title'      => pm_Locale::lmsg('overviewMonitorLast360days'),
                     'noEscape'   => true,
                     'searchable' => false,
                     'sortable'   => false
@@ -78,5 +86,46 @@ class Modules_UptimeRobot_List_Monitors
         $monitorsList->setDataUrl(array('action' => 'monitorslist-data'));
 
         return $monitorsList;
+    }
+
+    /**
+     * @param $monitorType
+     *
+     * @return string
+     */
+    private static function getHumandReadableMonitorType($monitorType){
+        switch ($monitorType) {
+            case 1:
+                return pm_Locale::lmsg('overviewMonitorTypeHttp');
+            case 1:
+                return pm_Locale::lmsg('overviewMonitorTypeKeyword');
+            case 2:
+                return pm_Locale::lmsg('overviewMonitorTypePing');
+            case 3:
+                return pm_Locale::lmsg('overviewMonitorTypePort');
+            default:
+                return pm_Locale::lmsg('overviewMonitorTypeUnknown');
+        }
+    }
+
+    /**
+     * @param $monitorType
+     * @param $monitorUrl     
+     *
+     * @return string Returns a hyperref or just text depending on the $monitorType.
+     */
+    public static function getHumandReadableURL($monitorType, $monitorUrl){
+
+        // 3 => Ping
+        if ($monitorType === 3) {
+            return $monitorUrl;
+        }
+
+        // Starts with http or https
+        if(substr($monitorUrl, 0, 7) == 'http://' || substr($monitorUrl, 0, 8) == 'https://'){
+            return '<a href="'.$monitorUrl.'" target="_blank">'.$monitorUrl.'</a>';
+        }
+
+        return $monitorUrl;
     }
 }
